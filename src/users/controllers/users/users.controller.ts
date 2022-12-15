e5b3b1dc9748a642e7cf114cce99e0d96b53c798
JamiuKolawole04 <Jakko4real@gmail.com>
2022-12-15 03:57:19 +0100

    feat: nest validation done and moving on to services

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  ValidationPipe,
+  UsePipes,
+  ParseIntPipe,
+  ParseBoolPipe,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+import { CreateUserDto } from 'src/users/dtos/createUsers.dto';
 
 @Controller('users')
 export class UsersController {
@@ -32,8 +48,41 @@ export class UsersController {
     ];
   }
 
+  //   @Post()
+  //   createUer(@Req() request: Request, @Res() response: Response) {
+  //     console.log(request.body);
+  //     response.send(request.body);
+  //   }
   @Post()
-  createUer() {
-    return {};
+  @UsePipes(new ValidationPipe())
+  createUer(@Body() userData: CreateUserDto) {
+    console.log(userData);
+    return userData;
+  }
+
+  //   @Get(':id')
+  //   getUserById(@Req() requset: Request, @Res() response: Response) {
+  //     console.log(requset.params);
+  //     response.send(requset.params);
+  //   }
+  @Get(':id')
+  getUserById(@Param('id', ParseIntPipe) id: number) {
+    console.log(id);
+    return { id };
+  }
+
+  @Get(':id/:postId')
+  getUserAndPostById(@Param('id') id: string, @Param('postId') postId: string) {
+    console.log({ id, postId });
+    return { id, postId };
+  }
+
+  @Get('driver/sort/query')
+  getDriversByQuery(
+    @Query('sortDesc', ParseBoolPipe) sortDesc: boolean,
+    @Query('id', ParseIntPipe) id: number,
+  ) {
+    console.log(sortDesc);
+    return sortDesc;
   }
 }
